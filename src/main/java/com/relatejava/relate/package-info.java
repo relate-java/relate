b11275ab02ece7e7be6b755eec071b41686a5530
ilmo.euro <ilmo.euro@gmail.com>
2015-11-02 18:19:31 +0200

    ** Added package-level documentation

--- src/main/java/com/relatejava/relate/package-info.java
+++ src/main/java/com/relatejava/relate/package-info.java
@@ -13,4 +13,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * <p>From relations to object graphs - a relation mapper for POJOs.</p>
+ * 
+ * <p>This package contains mappers that take relational data as input and produce
+ * object graphs based on them. The data can originate anywhere, but the most
+ * typical use-case is result sets produced by joining tables in a relational
+ * database. The package is type-safe, so there's multiple mappers with
+ * different child-parent relationships and different number of type parameters.</p>
+ * 
+ * <p><b>Naming scheme of classes:</b></p>
+ * <pre>
+ * 
+ * RelationMapper_a__b_c_d_e
+ * 
+ * a = number of children of each root object
+ * b = number of sub-children of first child
+ * c = number of sub-children of second child
+ * d = number of sub-children of third child
+ * e = number of sub-children of fourth child
+ * </pre>
+ * 
+ * <p><b>Usage example:</b></p>
+ * 
+ * <pre>
+ * 
+ * List&lt;List&lt;String&gt;&gt; values = 
+ *      Arrays.asList(
+ *          Arrays.asList("Accounting",     "Mark",    "mark@example.com"),
+ *          Arrays.asList("Accounting",     "Mark",    "mark.kish@example.com"),
+ *          Arrays.asList("Accounting",     "Jen",     "jen@example.com"),
+ *          Arrays.asList("IT",             "Fred",    "fred@example.com"),
+ *          Arrays.asList("IT",             "John",    "john@example.com"),
+ *          Arrays.asList("IT",             "John",    "john.doe@example.com"));
+ * 
+ * // Root is department name, subobject is person, sub-subobject is email
+ * RelationMapper_1__1&lt;String, String, String&gt;
+ *      mapper = new RelationMapper_1__1&lt;&gt;();
+ * for (List&lt;String&gt; row : values) {
+ *      // The following 2 lines are optional in this case
+ *      mapper.root(row.get(0));
+ *      mapper.relate_1(row.get(0), row.get(1));
+ *      mapper.relate_1_1(row.get(0), row.get(1), row.get(2));
+ * }
+ * 
+ * class Department {
+ *      public final String name;
+ *      public final Map&lt;String, Set&lt;String&gt;&gt; personnel;
+ * 
+ *      public Department(String name, Map&lt;String, Set&lt;String&gt;&gt; personnel) {
+ *          this.name = name;
+ *          this.personnel = personnel;
+ *      }
+ * }
+ * 
+ * // Java 8 syntax, works with 7 too
+ * List&lt;Department&gt; result = mapper.&lt;Department&gt;build(Department::new);
+ * </pre>
+ * 
+ */
 package com.relatejava.relate;
