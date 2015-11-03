38241958d3b754f7308bee788bfbd59018bfb846
ilmo.euro <ilmo.euro@gmail.com>
2015-11-03 10:45:17 +0200

    ** Improved documentation

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 **From relations to object graphs - a relation mapper for POJOs.**
 
-This package contains mappers that take relational data as input and produce object graphs based on them. The data can originate anywhere, but the most typical use-case is result sets produced by joining tables in a relational database. The package is type-safe, so there's multiple mappers with different child-parent relationships and different number of type parameters.
+This is a package package for mapping relational data to object
+graphs. The data can originate anywhere, but the most typical use-case
+is result sets produced by joining tables in a relational database. The
+mapping is type-safe, so there's multiple mappers with different
+child-parent relationships and different number of type parameters.
 
 **Naming scheme of classes:**
 
@@ -15,35 +19,45 @@ This package contains mappers that take relational data as input and produce obj
 **Usage example:**
 
  ```java
- List<List<String>> values = 
-      Arrays.asList(
-          Arrays.asList("Accounting",     "Mark",    "mark@example.com"),
-          Arrays.asList("Accounting",     "Mark",    "mark.kish@example.com"),
-          Arrays.asList("Accounting",     "Jen",     "jen@example.com"),
-          Arrays.asList("IT",             "Fred",    "fred@example.com"),
-          Arrays.asList("IT",             "John",    "john@example.com"),
-          Arrays.asList("IT",             "John",    "john.doe@example.com"));
+ import java.util.List;
+ import java.util.Arrays;
+ import com.relatejava.relate.RelationMapper_1__1;
  
- // Root is department name, subobject is person, sub-subobject is email
- RelationMapper_1__1<String, String, String>
-      mapper = new RelationMapper_1__1<>();
- for (List<String> row : values) {
-      // The following 2 lines are optional in this case
-      mapper.root(row.get(0));
-      mapper.relate_1(row.get(0), row.get(1));
-      mapper.relate_1_1(row.get(0), row.get(1), row.get(2));
+ public class RelateExample {
+ 
+     public static class Department {
+          public final String name;
+          public final Map<String, Set<String>> personnel;
+     
+          public Department(String name, Map<String, Set<String>> personnel) {
+              this.name = name;
+              this.personnel = personnel;
+          }
+     }
+ 
+     public static void main(String[] args) {
+         List<List<String>> values = 
+              Arrays.asList(
+                  Arrays.asList("Accounting",     "Mark",    "mark@example.com"),
+                  Arrays.asList("Accounting",     "Mark",    "mark.kish@example.com"),
+                  Arrays.asList("Accounting",     "Jen",     "jen@example.com"),
+                  Arrays.asList("IT",             "Fred",    "fred@example.com"),
+                  Arrays.asList("IT",             "John",    "john@example.com"),
+                  Arrays.asList("IT",             "John",    "john.doe@example.com"));
+         
+         // Root is department name, subobject is person, sub-subobject is email
+         RelationMapper_1__1<String, String, String>
+              mapper = new RelationMapper_1__1<>();
+         for (List<String> row : values) {
+              // The following 2 lines are optional in this case
+              mapper.root(row.get(0));
+              mapper.relate_1(row.get(0), row.get(1));
+              mapper.relate_1_1(row.get(0), row.get(1), row.get(2));
+         }
+         
+         // Java 8 syntax, works with 7 too
+         List<Department> result = mapper.<Department>build(Department::new);
+     }
+ 
  }
- 
- class Department {
-      public final String name;
-      public final Map<String, Set<String>> personnel;
- 
-      public Department(String name, Map<String, Set<String>> personnel) {
-          this.name = name;
-          this.personnel = personnel;
-      }
- }
- 
- // Java 8 syntax, works with 7 too
- List<Department> result = mapper.<Department>build(Department::new);
 ```
--- src/main/java/com/relatejava/relate/package-info.java
+++ src/main/java/com/relatejava/relate/package-info.java
@@ -17,11 +17,11 @@
 /**
  * <p>From relations to object graphs - a relation mapper for POJOs.</p>
  * 
- * <p>This package contains mappers that take relational data as input and produce
- * object graphs based on them. The data can originate anywhere, but the most
- * typical use-case is result sets produced by joining tables in a relational
- * database. The mapping is type-safe, so there's multiple mapper classes with
- * different child-parent relationships and different number of type parameters.</p>
+ * <p>This is a package package for mapping relational data to object
+ * graphs. The data can originate anywhere, but the most typical use-case
+ * is result sets produced by joining tables in a relational database. The
+ * mapping is type-safe, so there's multiple mappers with different
+ * child-parent relationships and different number of type parameters.</p>
  * 
  * <p><b>Naming scheme of classes:</b></p>
  * <pre>
@@ -39,37 +39,47 @@
  * 
  * <pre>
  * 
- * List&lt;List&lt;String&gt;&gt; values = 
- *      Arrays.asList(
- *          Arrays.asList("Accounting",     "Mark",    "mark@example.com"),
- *          Arrays.asList("Accounting",     "Mark",    "mark.kish@example.com"),
- *          Arrays.asList("Accounting",     "Jen",     "jen@example.com"),
- *          Arrays.asList("IT",             "Fred",    "fred@example.com"),
- *          Arrays.asList("IT",             "John",    "john@example.com"),
- *          Arrays.asList("IT",             "John",    "john.doe@example.com"));
+ * import java.util.List;
+ * import java.util.Arrays;
+ * import com.relatejava.relate.RelationMapper_1__1;
  * 
- * // Root is department name, subobject is person, sub-subobject is email
- * RelationMapper_1__1&lt;String, String, String&gt;
- *      mapper = new RelationMapper_1__1&lt;&gt;();
- * for (List&lt;String&gt; row : values) {
- *      // The following 2 lines are optional in this case
- *      mapper.root(row.get(0));
- *      mapper.relate_1(row.get(0), row.get(1));
- *      mapper.relate_1_1(row.get(0), row.get(1), row.get(2));
+ * public class RelateExample {
+ * 
+ *     public static class Department {
+ *          public final String name;
+ *          public final Map&lt;String, Set&lt;String&gt;&gt; personnel;
+ *     
+ *          public Department(String name, Map&lt;String, Set&lt;String&gt;&gt; personnel) {
+ *              this.name = name;
+ *              this.personnel = personnel;
+ *          }
+ *     }
+ * 
+ *     public static void main(String[] args) {
+ *         List&lt;List&lt;String&gt;&gt; values = 
+ *              Arrays.asList(
+ *                  Arrays.asList("Accounting",     "Mark",    "mark@example.com"),
+ *                  Arrays.asList("Accounting",     "Mark",    "mark.kish@example.com"),
+ *                  Arrays.asList("Accounting",     "Jen",     "jen@example.com"),
+ *                  Arrays.asList("IT",             "Fred",    "fred@example.com"),
+ *                  Arrays.asList("IT",             "John",    "john@example.com"),
+ *                  Arrays.asList("IT",             "John",    "john.doe@example.com"));
+ *         
+ *         // Root is department name, subobject is person, sub-subobject is email
+ *         RelationMapper_1__1&lt;String, String, String&gt;
+ *              mapper = new RelationMapper_1__1&lt;&gt;();
+ *         for (List&lt;String&gt; row : values) {
+ *              // The following 2 lines are optional in this case
+ *              mapper.root(row.get(0));
+ *              mapper.relate_1(row.get(0), row.get(1));
+ *              mapper.relate_1_1(row.get(0), row.get(1), row.get(2));
+ *         }
+ *         
+ *         // Java 8 syntax, works with 7 too
+ *         List&lt;Department&gt; result = mapper.&lt;Department&gt;build(Department::new);
+ *     }
+ * 
  * }
- * 
- * class Department {
- *      public final String name;
- *      public final Map&lt;String, Set&lt;String&gt;&gt; personnel;
- * 
- *      public Department(String name, Map&lt;String, Set&lt;String&gt;&gt; personnel) {
- *          this.name = name;
- *          this.personnel = personnel;
- *      }
- * }
- * 
- * // Java 8 syntax, works with 7 too
- * List&lt;Department&gt; result = mapper.&lt;Department&gt;build(Department::new);
  * </pre>
  * 
  */
